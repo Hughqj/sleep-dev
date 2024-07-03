4229c994268da5d16707607920a67764f5ec1c0d
huangquanjie <huangquanjie@starblink.co>
2024-07-03 15:44:05 +0800

    add

---
+++ src/main/java/com/example/sleepdev/SleepDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.sleepdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleepDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sleepdev/SleepDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sleepdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleepDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
